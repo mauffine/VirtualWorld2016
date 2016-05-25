825777a11ecebdf52614af2b0ced8a5e3b5ba466
Sean Mauff <sjmauff@hotmail.com>
2016-05-25 10:55:29 +1000

    added functions into Physics Scene

--- Virtual World 2016/src/Physics Objects/Physics Scene.h
+++ Virtual World 2016/src/Physics Objects/Physics Scene.h
@@ -1,18 +1,24 @@
 #pragma once
+#include <vector>
+
+#include "glm\glm.hpp"
+#include "glm\ext.hpp"
+
+#include "Physics Objects\PhysicsObjectBase.h"
 class PhysicsScene
 {
 public:
 	PhysicsScene();
 	~PhysicsScene();
 
+	glm::vec3 m_gravity;
+	float m_timestep;
+	std::vector<PhysicsScene*> m_actors;
+	void AddActor(PhysicsObjectBase*);
+	void RemoveActor(PhysicsObjectBase);
+	void Update();
+	void DebugScene();
+	void AddGizmos();
 private:
 
 };
-
-PhysicsScene::PhysicsScene()
-{
-}
-
-PhysicsScene::~PhysicsScene()
-{
-}
